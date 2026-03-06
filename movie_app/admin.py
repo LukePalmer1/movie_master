3a342859541e7ccf9b7d42ac44273afc23d6104e
Luke Palmer <3003736P@student.gla.ac.uk>
2026-03-06 14:45:17 +0000

    Addition of Models

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from movie_app.models import Movie, UserProfile, Rating
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "release_date", "poster_path", "average_rating")
+    exclude = ("movieID",)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("user_profile", "movie", "rating", "review")
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rating, RatingAdmin)
--- movie_app/models.py
+++ movie_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    movieID = models.IntegerField(unique=True, primary_key=True)
+    title = models.CharField(max_length=200)
+    release_date = models.CharField(max_length=10)
+    overview = models.CharField(max_length=1000)
+    poster_path = models.URLField(max_length=100)
+    average_rating = models.FloatField(max_length=4, default=0)
+
+    def __str__(self):
+        return self.title
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.CharField(max_length=500)
+    watch_list = models.ManyToManyField(Movie, blank=True)
+    follow_list = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+        
+class Rating(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET(-1)) # Set id = -1, will be handled later as q deleted user
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE) 
+    rating = models.IntegerField()
+    review = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.user_profile}'s review of {self.movie}"
